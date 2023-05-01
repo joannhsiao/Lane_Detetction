d62978d68544a901cacc0d4198875073115a4909
Joann <59599987+joannhsiao@users.noreply.github.com>
2023-05-01 16:53:18 +0800

    update test file

--- demo.py
+++ demo.py
@@ -10,3 +10,18 @@ image = "Data/video_images/30427_hd_Trim_Trim/150.jpg"
 img = cv2.imread(image)
 
 right_line, left_line, direction, highest_point, included_angle = img_seg(img)
+
+
+# x1, y1, x2, y2 = left_line
+# x3, y3, x4, y4 = right_line
+
+# cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+# cv2.circle(img, (x1, y1), 5, (255, 0, 0), 0)
+# cv2.circle(img, (x2, y2), 5, (0, 255, 0), 0)
+# cv2.line(img, (x3, y3), (x4, y4), (0, 0, 255), 2)
+# cv2.circle(img, (x3, y3), 5, (255, 0, 0), 0)
+# cv2.circle(img, (x4, y4), 5, (0, 255, 0), 0)
+
+# cv2.imshow("img", img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
